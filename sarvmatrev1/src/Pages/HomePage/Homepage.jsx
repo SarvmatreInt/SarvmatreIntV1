768a1e7fb72f73dd2f50c7daa6e24f10e34925f1
bansalgautam <gautamdevelops@gmail.com>
2023-08-06 14:19:52 +0530

    Fixed Homepage overflow

--- sarvmatrev1/src/Pages/HomePage/Homepage.jsx
+++ sarvmatrev1/src/Pages/HomePage/Homepage.jsx
@@ -12,7 +12,7 @@ import Tagline from "./HomePageComponant/tagLine/Tagline"
 
 const Homepage = () => {
   return (
-    <div className="mx-auto p-0 home-style">
+    <div className="mx-auto p-0 home-style overflow-x-hidden">
       <IntoSec/>
       <Tagline/>
       <AboutSection/>
