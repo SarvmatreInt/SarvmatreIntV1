9c1e097fc98b1a371e61e5962b588e4fe2a8153d
Varidh Srivastav <varidhsrivastava19145@gmail.com>
2023-08-07 06:25:58 +0530

    updated our history

--- sarvmatrev1/src/Pages/OurHistory/OurHistory.jsx
+++ sarvmatrev1/src/Pages/OurHistory/OurHistory.jsx
@@ -4,32 +4,30 @@ const OurHistory = () => {
       <h1 className="text-center text-5xl font-bold my-16">OUR HISTORY</h1>
       <div className="w-[95%] mx-auto">
         <p className="text-justify my-2">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam
-          laudantium ipsam totam officiis ipsa, minima debitis nemo et dolor
-          esse nulla illo dolorem qui quidem quos a architecto quasi voluptates
-          quisquam. Odit asperiores doloribus fugit consectetur distinctio,
-          minima eos rem? Officiis tempora perspiciatis illo, error quibusdam
-          repellendus consequuntur, obcaecati asperiores autem rerum dolores,
-          dignissimos exercitationem doloribus explicabo cum. Assumenda,
-          obcaecati? Quaerat fuga dicta autem quos eum. Rerum repellat labore
-          suscipit ea est aspernatur, dignissimos minima delectus reprehenderit?
-          Magnam, culpa architecto ratione dolorem ipsam illum distinctio cumque
-          consectetur beatae ea reprehenderit, amet ad perferendis. Harum
-          quisquam temporibus labore beatae praesentium vero?
+          Our history is a story of innovation and growth, of overcoming
+          challenges and seizing opportunities. We began our entrepreneurial
+          journey as a “Proprietorship firm” named as “Shiv Shakti Traders” over
+          a decade ago, dealing with various products and categories in offline
+          supply chain mode. As the years went by, we gained expertise and
+          market knowledge, acquiring an in-depth understanding of common
+          business practices and problems prevalent in the actual business
+          world. We take pride in our achievements, but remain committed to
+          pushing the boundaries of what is possible, and building a better
+          future for all our stakeholders.
         </p>
         <p className="text-justify my-2">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit.
-          Necessitatibus placeat doloribus veritatis dolores ipsam rerum quos
-          earum exercitationem laboriosam molestias quibusdam, aspernatur veniam
-          debitis! Excepturi impedit quo maxime, culpa explicabo commodi,
-          nostrum deserunt non officiis quidem magni minus eaque distinctio aut
-          voluptate labore porro ut exercitationem! Cupiditate expedita aut
-          totam temporibus eligendi ab voluptates recusandae? Magnam placeat cum
-          quaerat nam corrupti minus vel tempora aperiam, unde repudiandae rem
-          earum molestias dicta dolores maxime. Quos magnam eum, accusantium
-          architecto ut iste dolor cumque tempore necessitatibus nulla, alias in
-          beatae suscipit quisquam est illum ratione facere? Odit unde ipsam
-          autem aliquam esse!
+          Over the years, we have established connections with a diverse range
+          of businesses, from small-scale industries and MSMEs to farmers,
+          artisans, societies, SHGs, and local unorganized businesses. With a
+          mission to enable businesses rather than disrupting them, Sarvmatre
+          International intends to adopt a sector-agnostic approach to expansion
+          within India and globally, aligning with the government's vision of
+          boosting local businesses, "Vocal for Local" and "Make in India." The
+          company's platform will provide end-to-end solutions for multiple
+          business needs and scalability, from sourcing to selling, logistics,
+          financial, legal assistance. Our company aims to provide access to a
+          platform harnessing the power of technology, providing intelligent and
+          efficient decision-making.
         </p>
       </div>
     </div>
