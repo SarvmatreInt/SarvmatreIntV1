abf4dbd4d12ffd14251102a335f5fb61bd2e51d1
bansalgautam <gautamdevelops@gmail.com>
2023-08-07 23:20:43 +0530

    About us & Scrollbar Issue Solved

--- sarvmatrev1/src/Pages/HomePage/HomePageComponant/tagLine/Tagline.jsx
+++ sarvmatrev1/src/Pages/HomePage/HomePageComponant/tagLine/Tagline.jsx
@@ -1,6 +1,6 @@
 const Tagline = () => {
   return (
-    <div className="bg-primary1">
+    <div className="bg-primary1 overflow-y-hidden">
         <div className="container">
             <div className="py-5"> 
             <h1 className="text-center text-3xl sm:text-5xl font-bold text-white ">
