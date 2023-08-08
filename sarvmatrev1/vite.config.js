8317ce83fcb28ae2c7f9ffe18f259e2f86c09293
bansalgautam <gautamdevelops@gmail.com>
2023-08-08 23:18:22 +0530

    Configured Vite

--- sarvmatrev1/vite.config.js
+++ sarvmatrev1/vite.config.js
@@ -1,7 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import { resolve } from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    rollupOptions: {
+      main: resolve(__dirname, "index.html"),
+      404: resolve(__dirname, "index.html"),
+    },
+  },
+});
