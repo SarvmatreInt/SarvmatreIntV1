1cf75f861d8fcdb35b6ac2bfe4531aa55b318319
bansalgautam <gautamdevelops@gmail.com>
2023-08-08 23:22:57 +0530

    Restore to Default Settings

--- sarvmatrev1/vite.config.js
+++ sarvmatrev1/vite.config.js
@@ -1,14 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
-import { resolve } from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      main: resolve(__dirname, "index.html"),
-      404: resolve(__dirname, "index.html"),
-    },
-  },
 });
